2e9749fa69a64f3e81a1a08e6dc079f885cf9fb3
brandonnly <brandon@brandonly.me>
2020-12-03 01:06:38 -0500

    finished day3

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,17 +1,29 @@
 from rich import print
 import rich.traceback
+import math
 
 rich.traceback.install()
 
 area = [i for i in open("Day 3\input.txt", "r")]
 
-trees1 = 0
-xCoord = 0
 
-for n in range(0, len(area)):
-    if area[n][xCoord % len(area[0].strip())] == '#':
-        trees1 += 1
-    xCoord += 3
+def get_slope(area, deltaX, deltaY):
+    trees = 0
+    xCoord = 0
+
+    for n in range(0, len(area), deltaY):
+        if area[n][xCoord % len(area[0].strip())] == '#':
+            trees += 1
+        xCoord += deltaX
+
+    return trees
 
 
-print(f"Part 1 Answer: [green]{trees1}")
+deltas = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+pt2 = []
+for x in deltas:
+    pt2.append(get_slope(area, x[0], x[1]))
+
+
+print(f"Part 1 Answer: [green]{get_slope(area, deltas[1][0], deltas[1][1])}")
+print(f"Part 2 Answer: [green]{math.prod(pt2)}")
